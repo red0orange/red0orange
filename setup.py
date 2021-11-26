08f4b66cb78688ceb3d16886936e1401164994d1
red0orange <20183231102@m.scnu.edu.cn>
2021-11-26 19:30:01 +0800

    1. bug fix

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="red0orange", # Replace with your own username
-    version="1.0.0",
+    version="1.0.1",
     author="red0orange",
     author_email="huangdehao919@gmail.com",
     description="red0orange first pypi package",
